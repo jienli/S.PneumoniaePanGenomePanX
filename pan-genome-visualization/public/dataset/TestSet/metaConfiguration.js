78420aabc2514acfd9e84b48f6bc258b2069389c
Jien Li <jli.brendan@gmail.com>
2022-02-04 13:58:16 -0500

    added all files, this should work now

---
+++ pan-genome-visualization/public/dataset/TestSet/metaConfiguration.js
@@ -0,0 +1,3 @@
+var meta_details={"collection_date": ["unknown"], "country": ["unknown"], "host": ["unknown"]},
+meta_display={"meta_display_order": ["collection_date", "country", "host"], "color_options": {"collection_date": {"menuItem": "collection_date", "type": "continuous"}, "country": {"menuItem": "country", "type": "discrete"}, "host": {"menuItem": "host", "type": "discrete"}}};
+var association_columns=[];
